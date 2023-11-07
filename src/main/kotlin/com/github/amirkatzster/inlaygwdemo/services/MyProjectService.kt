f8ddb68ccc44d22cf5f39a790e4733910f6f44c1
GitHub Action <action@github.com>
2023-11-07 15:23:55 +0000

    Template cleanup

--- src/main/kotlin/com/github/amirkatzster/inlaygwdemo/services/MyProjectService.kt
+++ src/main/kotlin/com/github/amirkatzster/inlaygwdemo/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.amirkatzster.inlaygwdemo.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.amirkatzster.inlaygwdemo.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
